5e00b47793d09444e132aec6b82135840a934abf
riel <riel>
2004-06-28 12:51:05 +0000

    snapshots available from ftp://ftp.nl.linux.org/pub/spamikaze/snapshots/

--- html/download.php
+++ html/download.php
@@ -16,22 +16,15 @@ This is the first alpha release of Spamikaze. The software works and
 is in production on our own systems, but there are some rough edges
 left, as well as many TODO items.
 
-<p>Get it via
-<!-- <a href="/pub/spamikaze-0.1.tar.gz">http</a> or -->
-<a href="ftp://ftp.nl.linux.org/pub/spamikaze/spamikaze-0.1.tar.gz">ftp</a>.
+<p>Get
+<a href="ftp://ftp.nl.linux.org/pub/spamikaze/spamikaze-0.1.tar.gz">spamikaze-0.1.tar.gz</a> via ftp.
 
-<h2>Anonymous CVS</h2>
+<h2>CVS snapshots</h2>
 
-Spamikaze development can be followed up to the minute via anonymous
-CVS. Note that there could be experimental things in CVS, though, so
-only use a version from CVS if you know what you're doing.
-
-<p><tt>
-$ cvs -d :pserver:cvs@nl.linux.org:/home/CVS login<br>
-(Logging in to cvs@nl.linux.org)<br>
-CVS password: cvs<br>
-$ cvs -d :pserver:cvs@nl.linux.org:/home/CVS co spamikaze
-</tt>
+Spamikaze development can be followed by getting daily CVS snapshots
+from Spamikaze's <a href="ftp://ftp.nl.linux.org/pub/spamikaze/">ftp
+directory</a>.  Note that there often is experimental code in CVS, so
+only use these snapshots if you know what you're doing.
 
 <p>Want to help Spamikaze development? Subscribe to the mailing list and
 help us out...
