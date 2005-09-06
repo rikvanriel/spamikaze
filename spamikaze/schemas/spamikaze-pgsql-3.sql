ffdbe283df27c12fbde52c17d4d85fbe83f9de78
riel <riel>
2005-09-06 01:27:48 +0000

    add new (untested) postgres schema and database module

---
+++ spamikaze/schemas/spamikaze-pgsql-3.sql
@@ -0,0 +1,265 @@
+--
+-- PostgreSQL database dump
+--
+
+SET client_encoding = 'SQL_ASCII';
+SET check_function_bodies = false;
+
+SET SESSION AUTHORIZATION 'postgres';
+
+--
+-- TOC entry 4 (OID 2200)
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+SET SESSION AUTHORIZATION 'postgres';
+
+SET search_path = public, pg_catalog;
+
+--
+-- TOC entry 5 (OID 315297)
+-- Name: summaries; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE summaries (
+    summ_interval interval NOT NULL,
+    from_table text NOT NULL,
+    to_table text
+);
+
+
+--
+-- TOC entry 6 (OID 315297)
+-- Name: summaries; Type: ACL; Schema: public; Owner: postgres
+--
+
+REVOKE ALL ON TABLE summaries FROM PUBLIC;
+GRANT SELECT ON TABLE summaries TO querystats;
+GRANT SELECT ON TABLE summaries TO psbl;
+
+
+SET SESSION AUTHORIZATION 'postgres';
+
+--
+-- TOC entry 7 (OID 359560522)
+-- Name: qs_hourly; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE qs_hourly (
+    querytime timestamp without time zone DEFAULT ('now'::text)::timestamp(6) with time zone NOT NULL,
+    ip inet NOT NULL,
+    count integer NOT NULL
+);
+
+
+--
+-- TOC entry 8 (OID 359560522)
+-- Name: qs_hourly; Type: ACL; Schema: public; Owner: postgres
+--
+
+REVOKE ALL ON TABLE qs_hourly FROM PUBLIC;
+GRANT INSERT,SELECT,DELETE ON TABLE qs_hourly TO querystats;
+GRANT SELECT ON TABLE qs_hourly TO psbl;
+
+
+SET SESSION AUTHORIZATION 'postgres';
+
+--
+-- TOC entry 9 (OID 359560525)
+-- Name: qs_monthly; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE qs_monthly (
+    querytime timestamp without time zone DEFAULT ('now'::text)::timestamp(6) with time zone NOT NULL,
+    ip inet NOT NULL,
+    count integer NOT NULL
+);
+
+
+--
+-- TOC entry 10 (OID 359560525)
+-- Name: qs_monthly; Type: ACL; Schema: public; Owner: postgres
+--
+
+REVOKE ALL ON TABLE qs_monthly FROM PUBLIC;
+GRANT INSERT,SELECT,DELETE ON TABLE qs_monthly TO querystats;
+GRANT SELECT ON TABLE qs_monthly TO psbl;
+
+
+SET SESSION AUTHORIZATION 'postgres';
+
+--
+-- TOC entry 11 (OID 359734272)
+-- Name: qs_daily; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE qs_daily (
+    querytime timestamp without time zone DEFAULT ('now'::text)::timestamp(6) with time zone NOT NULL,
+    ip inet NOT NULL,
+    count integer NOT NULL
+);
+
+
+--
+-- TOC entry 12 (OID 359734272)
+-- Name: qs_daily; Type: ACL; Schema: public; Owner: postgres
+--
+
+REVOKE ALL ON TABLE qs_daily FROM PUBLIC;
+GRANT INSERT,SELECT,DELETE ON TABLE qs_daily TO querystats;
+GRANT SELECT ON TABLE qs_daily TO psbl;
+
+
+SET SESSION AUTHORIZATION 'postgres';
+
+--
+-- TOC entry 13 (OID 361494746)
+-- Name: eventtypes; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE eventtypes (
+    id serial NOT NULL,
+    eventtext text
+);
+
+
+--
+-- TOC entry 14 (OID 361494746)
+-- Name: eventtypes; Type: ACL; Schema: public; Owner: postgres
+--
+
+REVOKE ALL ON TABLE eventtypes FROM PUBLIC;
+GRANT SELECT ON TABLE eventtypes TO psbl;
+
+
+--
+-- Data for TOC entry 6 (OID 361494746)
+-- Name: eventtypes; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY eventtypes (id, eventtext) FROM stdin;
+1	unknown
+2	removed through website
+3	received spamtrap mail
+4	major smtp violation
+\.
+
+
+SET SESSION AUTHORIZATION 'postgres';
+
+
+--
+-- TOC entry 15 (OID 361499222)
+-- Name: blocklist; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE blocklist (
+    ip inet,
+    expires timestamp without time zone
+);
+
+
+--
+-- TOC entry 16 (OID 361499226)
+-- Name: ipevents; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE ipevents (
+    ip inet,
+    eventtime timestamp without time zone,
+    eventid smallint
+);
+
+
+--
+-- TOC entry 19 (OID 359618684)
+-- Name: qs_monthly_ip_key; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX qs_monthly_ip_key ON qs_monthly USING btree (ip);
+
+
+--
+-- TOC entry 20 (OID 359618685)
+-- Name: qs_monthly_time_ip_key; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX qs_monthly_time_ip_key ON qs_monthly USING btree (querytime, ip);
+
+
+--
+-- TOC entry 17 (OID 359618686)
+-- Name: qs_hourly_ip_key; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX qs_hourly_ip_key ON qs_hourly USING btree (ip);
+
+
+--
+-- TOC entry 18 (OID 359618687)
+-- Name: qs_hourly_time_ip_key; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX qs_hourly_time_ip_key ON qs_hourly USING btree (querytime, ip);
+
+
+--
+-- TOC entry 21 (OID 359734275)
+-- Name: qs_daily_ip_key; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX qs_daily_ip_key ON qs_daily USING btree (ip);
+
+
+--
+-- TOC entry 22 (OID 359734276)
+-- Name: qs_daily_time_ip_key; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX qs_daily_time_ip_key ON qs_daily USING btree (querytime, ip);
+
+
+--
+-- TOC entry 24 (OID 361499224)
+-- Name: blocklist_ip_index; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX blocklist_ip_index ON blocklist USING btree (ip);
+
+
+--
+-- TOC entry 23 (OID 361499225)
+-- Name: blocklist_expires_index; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX blocklist_expires_index ON blocklist USING btree (expires);
+
+
+--
+-- TOC entry 26 (OID 361499228)
+-- Name: ipevents_ip_index; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ipevents_ip_index ON ipevents USING btree (ip);
+
+
+--
+-- TOC entry 25 (OID 361499229)
+-- Name: ipevents_eventtime_index; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ipevents_eventtime_index ON ipevents USING btree (eventtime);
+
+
+--
+-- TOC entry 3 (OID 2200)
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON SCHEMA public IS 'Standard public schema';
+
+
---
+++ spamikaze/scripts/Spamikaze/PgSQL_3.pm
@@ -0,0 +1,158 @@
+# Spamikaze::PgSQL_3.pm
+#
+# Copyright (C) 2005 Rik van Riel <riel@surriel.com>
+#
+# Released under the GNU GPL
+#
+# NO WARRANTY, see the file COPYING for details.
+#
+# This file is part of the spamikaze project:
+#     http://spamikaze.nl.linux.org/
+
+#
+# Database abstractions for the 3rd Postgres schema for Spamikaze
+#
+
+package Spamikaze::PgSQL_3;
+use strict;
+use warnings;
+use DBI;
+use Env qw( HOME );
+
+# Get all the IP addresses that are listed in the DNSBL.
+sub get_listed_addresses {
+	my @addresses = ();
+	my $dbh;
+	my $sql;
+	my $sth;
+	my $ip;
+
+	$dbh = Spamikaze::DBConnect();
+
+	$sql = "SELECT ip FROM blocklist";
+	$sth = $dbh->prepare($sql);
+	$sth->execute();
+	$sth->bind_columns( undef, \$ip );
+
+	while ( $sth->fetch() ) {
+		unshift(@addresses, $ip);
+	}
+	$sth->finish();
+	$dbh->disconnect();
+
+	return @addresses;
+}
+
+# This function gets called from ->storeip and ->removefromdb,
+# with a database connection already made!
+sub store_ipevent
+{
+    my ( $self, $dbh, $ip, $type )  = @_;
+    my $sql = "INSERT INTO ipevents (?, CURRENT_TIMESTAMP, eventid) SELECT
+			 eventid FROM eventtypes WHERE eventtext = '?'";
+
+    my $sth = $dbh->prepare($sql);
+    $sth->execute($ip, $type);
+    $sth->finish;
+}
+
+sub storeip
+{
+    my ( $self, $ip, $type )  = @_;
+    my $expires = time() + $Spamikaze::firsttime;
+    my $rv;
+
+    my $dbh = Spamikaze::DBConnect();
+    unless ($dbh->do("INSERT INTO blocklist ($ip, $expires)")) {
+        $dbh->do("UPDATE blocklist SET expires = $expires WHERE ip = $ip");
+    }
+    $self->store_ipevent($dbh, $ip, $type);
+    $dbh->commit();
+    $dbh->disconnect();
+}
+
+sub expire
+{
+    my ($self, @dontexpire) = @_;
+
+    my $dbh = Spamikaze::DBConnect();
+    $dbh->do("DELETE FROM blocklist WHERE expires < CURRENT_TIMESTAMP");
+    $dbh->commit();
+    $dbh->disconnect();
+}
+
+sub remove_from_db($)
+{
+	my ($self, $ip) = @_;
+	my $rows_affected;
+	my $dbh;
+
+	# DBI connect params.
+	$dbh = Spamikaze::DBConnect();
+        $rows_affected = $dbh->do("DELETE FROM blocklist WHERE ip = $ip");
+	if ($rows_affected > 0) {
+		$self->store_ipevent($dbh, $ip, "removed through website");
+	}
+        $dbh->commit();
+	$dbh->disconnect();
+
+	return $rows_affected;    
+}
+
+sub get_listing_info
+{
+	my ($self, $ip) = @_;
+	my %iplog = ();
+	my $eventtext;
+	my $listed = 0;
+        my $time;
+	my $dbh;
+	my $sth;
+        my $sql;
+    
+	$dbh = Spamikaze::DBConnect();
+
+	#
+	# First, get all the events for this IP address
+	#
+	$sql = "SELECT eventtime, eventtext FROM ipevents, eventtypes WHERE
+			ipevents.ip = ? AND ipevents.eventid = eventtypes.id";
+
+	$sth = $dbh->prepare($sql);
+	$sth->execute($ip);
+	$sth->bind_columns(undef, \$time, \$eventtext);
+	while ($sth->fetch()) {
+		$found++;
+		$iplog{$time} = $eventtext;
+	}
+	$sth->finish();
+
+	#
+	# is the IP currently listed?
+	#
+	$sql = "SELECT expires FROM blocklist WHERE ip = ?";
+	$sth = $dbh->prepare($sql);
+	$visible = $sth->execute($ip);
+	$sth->bind_columns(\$time);
+	while ($sth->fetch()) {
+		listed = 1;
+	}
+	$sth->finish();
+
+	$dbh->disconnect();
+
+	return ($listed, %iplog);
+}
+
+sub new {
+	my $class = shift;
+	my $self = {};
+	bless $self, $class;
+	return $self;
+}
+
+BEGIN {
+	# nothing
+}
+
+1;
